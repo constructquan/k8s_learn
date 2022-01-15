0e54c532aa014ae32abb6ef0efe201b202bb844e
ahchpr <hellochenpro@hotmail.com>
2022-01-15 21:37:47 +0800

    修改实现方式

--- module2/src/hello_http/hello_http.go
+++ module2/src/hello_http/hello_http.go
@@ -3,10 +3,11 @@ package main
 import (
 
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"net"
 
 )
 
@@ -19,41 +20,72 @@ func Log(handler http.HandlerFunc) http.HandlerFunc{
 }
 
 
+func index(w http.ResponseWriter, r *http.Request){
+	//w.Write([]byte("Hello world...GO"))
+	// 设置 VERSION
+	os.Setenv("VERSION", "v0.9.1")
+	version := os.Getenv("VERSION")
+	w.Header().Set("VERSION", version)
+	fmt.Printf("os VERSION: %s\n", version)
+	// 将request的header设置到response中
+	for name, headers := range r.Header{
+		for _, h := range headers {
+            fmt.Printf( "Header kye: %s, Header value:  %s\n", name, headers)
+			w.Header().Set(name, h)		
+        }	
+	}
+	// 打印访问日志ip
+	clientip  := getCurrentIP(r)
+	log.Printf( "Success! Response code: %d", 200)
+	log.Printf("Success: clientip: %s", clientip)
+
+}
+//健康检查
+func healthz(w http.ResponseWriter, r * http.Request){
+	fmt.Fprintf(w, "working")
+}
+
+func getCurrentIP(r *http.Request) string {
+	// 这里也可以通过X-Forwarded-For请求头的第一个值作为用户的ip
+	// 但是要注意的是这两个请求头代表的ip都有可能是伪造的
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+	   // 当请求头不存在即不存在代理时直接获取ip
+	   ip = strings.Split(r.RemoteAddr, ":")[0]
+	}
+	return ip
+ }
+
+// ClientIP 尽最大努力实现获取客户端 IP 的算法。
+// 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+	   return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+	   return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+	   return ip
+	}
+	return ""
+ }
 
 func main() {
+    mux := http.NewServeMux()
 	
-	http.HandleFunc("/header", Log(reqHeader))
-	http.HandleFunc("/version", Log(getVersion))
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("healthz", healthz)
+	// http.HandleFunc("/header", Log(reqHeader))
+	// http.HandleFunc("/version", Log(getVersion))
 
-	http.HandleFunc("/",  Log(Default))
-	http.HandleFunc("/healthz", healthz)
+	// http.HandleFunc("/",  Log(Default))
+	// http.HandleFunc("/healthz", healthz)
 
-	if err := http.ListenAndServe(":8080", nil);err != nil {
+	if err := http.ListenAndServe(":8080", mux);err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-}
-
-func Default(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Hello World... GO ...")
-}
-
-
-func reqHeader(w http.ResponseWriter, req *http.Request){
-	for name, headers := range req.Header {
-        for _, h := range headers {
-            //io.WriteString(w, fmt.Sprintf( "%v: %v\n", name, h))
-			w.Header().Add(name, h)		
-        }
-    }
-}
-
-func getVersion(w http.ResponseWriter, req *http.Request){
-	version := os.Getenv("VERSION")
-	w.Header().Add("VERSION", version)
-	io.WriteString(w, fmt.Sprintf("Version: %s\n", version))
-
-}
-
-func healthz(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "ok\n")
 }
