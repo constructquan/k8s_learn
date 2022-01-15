6abb16ed882cebbf1e767715c1bb74a3a0e4dc15
ahchpr <hellochenpro@hotmail.com>
2022-01-15 22:35:09 +0800

    提交一个httpserver的Dockerfile镜像制作

---
+++ module3/httpserver/hellohttp.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+)
+
+
+func Log(handler http.HandlerFunc) http.HandlerFunc{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Printf( "%s %s %d %s\n", r.RemoteAddr, r.Method, http.StatusOK, r.URL)
+        handler(w, r)
+    })
+}
+
+
+
+func main() {
+	
+	http.HandleFunc("/header", Log(reqHeader))
+	http.HandleFunc("/version", Log(getVersion))
+
+	http.HandleFunc("/",  Log(Default))
+	http.HandleFunc("/healthz", healthz)
+
+	if err := http.ListenAndServe(":8080", nil);err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func Default(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Hello World... GO ...")
+}
+
+
+func reqHeader(w http.ResponseWriter, req *http.Request){
+	for name, headers := range req.Header {
+        for _, h := range headers {
+            //io.WriteString(w, fmt.Sprintf( "%v: %v\n", name, h))
+			w.Header().Add(name, h)		
+        }
+    }
+}
+
+func getVersion(w http.ResponseWriter, req *http.Request){
+        os.Setenv("VERSION", "v1.0.0")
+	version := os.Getenv("VERSION")
+	w.Header().Add("VERSION", version)
+	io.WriteString(w, fmt.Sprintf("Version: %s\n", version))
+
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok\n")
+}
